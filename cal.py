66454ead89ace0b9840836b9506551fec09dc5a2
lebrancconvas <lebranccconvas@gmail.com>
2019-03-08 20:49:56 +0700

    :tada: Create Repository.

---
+++ cal.py
@@ -0,0 +1,20 @@
+# voter คือจำนวนผู้ไปใช้สิทธิเลือกตั้งทั้งหมด
+voter = int(input("Number of Voter: "))
+
+# mhr คือจำนวน ส.ส. 1 คนในพรรคที่จะได้เข้าไปนั่งในสภา
+mhr = voter / 500
+
+# all_vote คือ คะแนนรวมของพรรคการเมืองรวมทุกเขต
+all_vote = int(input("Vote Score of Party of mhr: "))
+
+# all_mhr คือ จำนวน ส.ส.พรรคทั้งหมดที่จะได้เข้าไปนั่งในสภา
+all_mhr = all_vote / mhr
+
+# dst_mht คือ จำนวนเขตที่ส.ส.ของพรรคชนะโหวต
+dst_mhr = int(input("All District that the party win: "))
+
+# ptl_mhr คือ จำนวน ส.ส.ปาร์ตี้ลิสต์ทั้งหมดที่พรรคจะได้มา
+ptl_mhr = all_mhr - dst_mhr
+
+print("มีจำนวนผู้ไปใช้สิทธิ {} คน\nสส 1 คนต้องใช้คะแนน {} โหวต\nพรรค ABC ได้คะแนนโหวตทั้งประเทศรวม {} โหวต\nดังนั้นพรรค ABC จะได้ สส เข้าไปนั่งในสภา {} คน\nพรรค ABC ชนะไปทั้งหมด {} เขต\nดังนั้นพรรค ABC จะได้ สส ปาร์ตี้ลิสต์ไปทั้งหมด {} คน".format(voter,mhr,all_vote,all_mhr,dst_mhr,ptl_mhr))
+
